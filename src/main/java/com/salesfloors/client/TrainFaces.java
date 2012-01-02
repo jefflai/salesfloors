479d4b57af38b05b125a9599a37782ac48600d47
Derek Tia <dtia@salesforce.com>
2012-01-02 12:48:10 -0800

    add faces controller

--- src/main/java/com/salesfloors/client/TrainFaces.java
+++ src/main/java/com/salesfloors/client/TrainFaces.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 public class TrainFaces {
 
-	public static final String oauthToken = "AAADMH1YfpJwBAMNUYyNSefXE2kL2HkMhomBZAtmFOEbUi0eQsaZBDE2vP4yIeVeY5YRpOuIz94ZCx33MtRQPzDIlPbMlz8F3wALuXSEowZDZD";
+	public static String oauthToken = "AAADMH1YfpJwBAMNUYyNSefXE2kL2HkMhomBZAtmFOEbUi0eQsaZBDE2vP4yIeVeY5YRpOuIz94ZCx33MtRQPzDIlPbMlz8F3wALuXSEowZDZD";
 	
 	public static final String apiKey = "eed2f515182570f4617551a8a7827188"; 
 	public static final String apiSecret = "de832406e48d700fd97956f98f800153"; 
@@ -21,14 +21,30 @@ public class TrainFaces {
 	public static final String faceDotComTrainURL = "http://api.face.com/faces/train.json?api_key={apiKey}&api_secret={apiSecret}&uids={userIds}&namespace={nameSpace}&user_auth={userAuth}&callback_url={callbackUrl}";
 	//public static final String faceDotComRecognizeURL = ""
 	
+	public static final String fbClientId = "224434960966812";
+	public static final String fbRedirectUri = "http://gentle-samurai-2258.herokuapp.com/faces/fb_oauth";
+	
+	public static final String facebookAuthURL = "https://www.facebook.com/dialog/oauth?client_id={fbClientId}&redirect_uri={fbRedirectUri}&response_type=token"; 
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		trainFace(desiredUserIds);		
+		getFbOauth();
+		System.out.println("new oauth token:" + oauthToken);
+		
+	}
+	
+	private static void getFbOauth() {
+		Map<String,String> vars = new HashMap<String,String>();
+		vars.put("fbClientId", fbClientId);
+		vars.put("fbRedirectUri", fbRedirectUri);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getForObject(facebookAuthURL, String.class, vars);		
 	}
  
-	private static void trainFace(String userIds) {
+	public static void trainFace(String userIds) {
 		Map<String,String> vars = new HashMap<String,String>();
 		vars.put("apiKey", apiKey);
 		vars.put("apiSecret", apiSecret);
--- src/main/java/com/salesfloors/client/controller/FacesController.java
+++ src/main/java/com/salesfloors/client/controller/FacesController.java
@@ -6,16 +6,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.salesfloors.client.TrainFaces;
+
 @Controller
 public class FacesController {
 	@RequestMapping(value = "/faces/fb_oauth", method = RequestMethod.POST)
     public ModelAndView getOauthToken(@RequestParam(value="access_token") String oauthToken)
-    		 {
-
+    {
 		ModelAndView mav = new ModelAndView();
     	mav.addObject("oauth_token", oauthToken);    	
-    	mav.setViewName("random_view");
-    	
+    	TrainFaces.oauthToken = oauthToken;
+    	TrainFaces.trainFace(TrainFaces.desiredUserIds);		
     	return mav;
     }
 }
