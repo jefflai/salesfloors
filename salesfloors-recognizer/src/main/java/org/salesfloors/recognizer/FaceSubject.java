99b4c4ca4177fdc0f41b7add7c27613608e4f3ba
Derek Tia <dtia@salesforce.com>
2012-01-09 00:51:27 -0800

    edit face subject enums

--- salesfloors-recognizer/src/main/java/org/salesfloors/recognizer/FaceSubject.java
+++ salesfloors-recognizer/src/main/java/org/salesfloors/recognizer/FaceSubject.java
@@ -2,8 +2,7 @@ package org.salesfloors.recognizer;
 
 public enum FaceSubject {
 	JEFF("1224055@facebook.com"),
-	DEREK("517172868@facebook.com"),
-	RAHUL("1906866@facebook.com"),
+	DEREK("517172868@facebook.com"),	
 	ELAINA("730592586@facebook.com")
 	;
 	
